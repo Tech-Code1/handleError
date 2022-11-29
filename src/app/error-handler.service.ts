fe496f5343bcce970b3c85250111a45273fbe87e
Deivi A <arocutipa17@gmail.com>
2022-11-28 20:45:09 -0500

    fix validate fields

--- src/app/error-handler.service.ts
+++ src/app/error-handler.service.ts
@@ -8,59 +8,54 @@ interface IErrorsKeys {
   textError: string;
 }
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ErrorHandlerService {
-
-  errorInput: string = ""
-  inputName: string = ""
+  errorInput: string = '';
+  inputName: string = '';
   //controls!: FormGroupControls;
   //errorName: AllValidationErrors | undefined | string;
   //error: AllValidationErrors | undefined | string = this.getFormValidationErrors(this.controls).shift()?.error_name;
 
-  constructor() {
+  constructor() {}
 
-  }
-
-  handlerErrorInputs(formName:  FormGroupControls, inputName: string, controlName: string): string {
-
-    const error: any = this.getFormValidationErrors(formName).shift();
-
-    const errorName = error!.error_name
-    const control = error?.control_name
-
+  handlerErrorInputs(
+    formName: FormGroupControls,
+    inputName: string,
+    controlName: string
+  ): string {
+    const error: any = this.getFormValidationErrors(formName).filter(
+      (e) => e.control_name === controlName
+    );
+    const errorName = error[0]!.error_name;
+    const control = error[0]?.control_name;
     if (control === controlName) {
       const listErrors: any = {
-        'required' : `El ${inputName} es requerido.`,
-        'minlength' : `El ${inputName} debe de tener mas de 3 caracteres.`,
-        'maxlength' : `El ${inputName} es demasiado extenso.`,
-        'pattern' : `Se requiere un ${inputName} valido.`,
-      }
+        required: `El ${inputName} es requerido.`,
+        minlength: `El ${inputName} debe de tener mas de 3 caracteres.`,
+        maxlength: `El ${inputName} es demasiado extenso.`,
+        pattern: `Se requiere un ${inputName} valido.`,
+      };
 
-      this.errorInput =  listErrors[errorName]
+      this.errorInput = listErrors[errorName];
 
-      return this.errorInput
+      return this.errorInput;
     }
 
-    return "Error desconocido"
+    return 'Error desconocido';
   }
 
   getFormValidationErrors(controls: FormGroupControls): AllValidationErrors[] {
-
     let errors: AllValidationErrors[] = [];
-
-    Object.keys(controls).forEach(key => {
-      const control = controls[ key ];
-      if (control instanceof FormGroup) {
-        errors = errors.concat(this.getFormValidationErrors(control.controls));
-      }
-      const controlErrors: ValidationErrors | null = controls[ key ].errors;
+    Object.keys(controls).map((key) => {
+      const control = controls[key].errors;
+      const controlErrors: ValidationErrors | null = control;
       if (controlErrors !== null) {
-        Object.keys(controlErrors).forEach(keyError => {
+        Object.keys(controlErrors).map((keyError) => {
           errors.push({
             control_name: key,
             error_name: keyError,
-            error_value: controlErrors[ keyError ]
+            error_value: controlErrors[keyError],
           });
         });
       }
